38f030b353282563c1ca12382a8588a63fdcc2d7
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-03-12 12:55:49 -0400

    First Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
     return (
         <div>
             <h1>Let's get started!</h1>
+            <p>This is also visible.</p>
+            <ExpenseItem></ExpenseItem>
         </div>
     );
 }
